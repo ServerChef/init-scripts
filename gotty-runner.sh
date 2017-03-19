07c3c6e281e41ea81381e72967107b4c5013a45a
root <root@jessie.raw>
2017-03-19 17:47:20 +0000

    Add environment to gotty

--- gotty-runner.sh
+++ gotty-runner.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+su -c '
+printenv
+
+echo "Ensuring that session exists"
 tmux has-session -t serverchef || tmux new-session -d -s serverchef
-export TERM=xterm-256color 
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin 
-gotty -w -a 127.0.0.1 -p 59764 tmux attach -t serverchef
+
+echo "Starting gotty process"
+export TERM=xterm-256color
+gotty -w -a 127.0.0.1 -p 59764 tmux new-session -A -s serverchef
+' - $(whoami)
